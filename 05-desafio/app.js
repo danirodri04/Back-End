bd424009a29b4cf425a2ed84682912ef545ac803
Daniel Rodriguez <danielrodri2304@gmail.com>
2023-04-26 21:46:18 -0300

    Correxion tarea

--- 05-desafio/app.js
+++ 05-desafio/app.js
@@ -1,22 +1,56 @@
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
-const exphbs = require('express-handlebars');
+const expressHandlebars = require('express-handlebars').create();
+
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-app.engine('handlebars', exphbs());
+
+app.engine('handlebars', expressHandlebars.engine);
 app.set('view engine', 'handlebars');
 app.use(express.static('public'));
 
+
+// Rutas y middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
 const products = [];
+
 
 app.get('/', (req, res) => {
     res.render('home', { products });
 });
 
+
 app.get('/realtimeproducts', (req, res) => {
     res.render('realTimeProducts', { products });
+});
+
+
+// Manejo de eventos de WebSocket
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+
+    socket.on('newProduct', (product) => {
+        products.push(product);
+        io.emit('updateProducts', products);
+    });
+
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+
+// Iniciar el servidor
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
